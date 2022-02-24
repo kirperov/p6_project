35f0be969a76ac00df1a37b75a267fd185dd5b00
Leatr <leatraore1@gmail.com>
2022-02-24 12:25:53 +0100

    add server.js/ package.json/app.js

---
+++ backend/app.js
@@ -0,0 +1,27 @@
+const express = require('express');
+
+const app = express();
+
+app.use('/api/sauces', (req, res, next) => {
+    const sauces = [
+      {
+        _id: 'oeihfzeoi',
+        title: 'Mon premier objet',
+        description: 'Les infos de mon premier objet',
+        imageUrl: 'https://cdn.pixabay.com/photo/2019/06/11/18/56/camera-4267692_1280.jpg',
+        price: 4900,
+        userId: 'qsomihvqios',
+      },
+      {
+        _id: 'oeihfzeomoihi',
+        title: 'Mon deuxième objet',
+        description: 'Les infos de mon deuxième objet',
+        imageUrl: 'https://cdn.pixabay.com/photo/2019/06/11/18/56/camera-4267692_1280.jpg',
+        price: 2900,
+        userId: 'qsomihvqios',
+      },
+    ];
+    res.status(200).json(sauces);
+  });
+
+module.exports = app;
