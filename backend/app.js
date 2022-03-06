750cfe32fc9e2e7091a6c9fc0f6e6129a0aa9db8
Leatr <leatraore1@gmail.com>
2022-03-06 13:25:43 +0100

    fix problem login/signup

--- backend/app.js
+++ backend/app.js
@@ -1,29 +1,25 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const mongoose = require('mongoose');
+const userRoutes = require("./routes/users");
 
-const saucesRoutes = require('./routes/sauces');
-const userRoutes = require('./routes/user');
-const app = express();
-
-mongoose.connect('mongodb+srv://LeaT:00000000000@cluster0.qmr69.mongodb.net/myFirstDatabase?retryWrites=true&w=majority',
+mongoose.connect('mongodb',
   { useNewUrlParser: true,
     useUnifiedTopology: true })
   .then(() => console.log('Connexion à MongoDB réussie !'))
   .catch(() => console.log('Connexion à MongoDB échouée !'));
 
-
-app.use(express.json());
+const app = express();
 
 app.use((req, res, next) => {
-  res.setHeader('Access-Control-Allow-Origin', '*');
-  res.setHeader('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content, Accept, Content-Type, Authorization');
-  res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, PATCH, OPTIONS');
-  next();
-});
+    res.setHeader('Access-Control-Allow-Origin', '*');
+    res.setHeader('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content, Accept, Content-Type, Authorization');
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, PATCH, OPTIONS');
+    next();
+  });
 
-app.use(bodyParser.json);
+app.use(bodyParser.json());
 
-app.use('/api/sauces', saucesRoutes);
 app.use('/api/auth', userRoutes);
+
 module.exports = app;
--- backend/routes/user.js
+++
@@ -1,9 +0,0 @@
-const express = require('express');
-const router = express.Router();
-
-const userCtrl = require('../controllers/user');
-
-router.post('/api/auth/signup', userCtrl.signup);
-router.post('/api/auth/login/', userCtrl.login);
-
-module.exports = router;
