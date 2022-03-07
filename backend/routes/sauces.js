7c9399597375734425e93d73fb7901aca475dfc3
Leatr <leatraore1@gmail.com>
2022-03-07 12:16:11 +0100

    Likes

--- backend/routes/sauces.js
+++ backend/routes/sauces.js
@@ -1,15 +1,19 @@
 const express = require('express');
 const router = express.Router();
-const saucesCtrl = require('../controllers/sauces');
+
+
+
 const auth = require('../middleware/auth');
 const multer = require('../middleware/multer-config');
 
-
+const saucesCtrl = require('../controllers/sauces');
+const likeCtrl = require('../controllers/likes');
 
 router.post('/', auth, multer, saucesCtrl.createSauce);
 router.put('/:id',auth, saucesCtrl.modifySauce );
 router.delete('/:id',auth, saucesCtrl.deleteSauce);
 router.get('/:id',auth, saucesCtrl.getOneSauce);
 router.get('/', auth, saucesCtrl.getAllSauces); 
+router.post('/:id/like', auth, likeCtrl.likeSauce);
 
 module.exports = router;
